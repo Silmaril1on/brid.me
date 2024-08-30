a4562a922897fda9a300d02b6f99e947806d3c51
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2024-08-30 15:08:44 +0400

    c

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "BRID | Home",
-  description: "Graphics design agency - Creative agency ",
+  description: "Creative Agency",
 };
 
 const myFont = localFont({ src: "../public/font/NeutralFace.otf" });
